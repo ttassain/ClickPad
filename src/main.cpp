33b04180eb3858ea96de685bf67f42c5f2f8f2c1
Thierry Tassain <thierry.tassain@orange.fr>
2022-12-31 17:44:46 +0100

    Clean code

--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,14 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 byte rowPins[ROWS] = {10, 16, 14, 15};
 byte colPins[COLS] = {6, 7, 8, 9};
 
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 unsigned long lastUpdate = 0;
 unsigned long clickDelay = 100;
@@ -39,19 +38,23 @@ uint8_t ledState = HIGH;
 
 bool bourinage = false;
 
-void initOled() {
+void initOled()
+{
 
-	if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-		Serial.println(F("SSD1306 allocation failed"));
-		for(;;);
-	}
-	display.clearDisplay();
-	display.setTextSize(2);
-	display.cp437(true); // https://en.wikipedia.org/wiki/Code_page_437
-	display.display();
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.cp437(true); // https://en.wikipedia.org/wiki/Code_page_437
+  display.display();
 }
 
-void refreshScreen() {
+void refreshScreen()
+{
   display.clearDisplay();
   display.setTextColor(SSD1306_BLACK);
   display.fillRect(0, 0, SCREEN_WIDTH, 16, SSD1306_WHITE);
@@ -65,7 +68,8 @@ void refreshScreen() {
   display.display();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   Keyboard.begin(KeyboardLayout_fr_FR);
@@ -74,64 +78,76 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   initOled();
- 
+
   refreshScreen();
 }
 
-void loop() {
+void loop()
+{
   char customKey = kpd.getKey();
-  if (customKey) {
-    switch (customKey) {
-      case 'A':
-        bourinage = false;
-        break;
-      case 'B':
-        bourinage = true;
-        break;
-      case '1':
-        if (clickDelay < 500) {
-          clickDelay += 10;
-        }
-        break;
-      case '4':
-        if (clickDelay > 10) {
-          clickDelay -= 10;
-        }
-        break;
-      case '2':
-        mouseButton = MOUSE_LEFT;
-        break;
-      case '5':
-        mouseButton = MOUSE_RIGHT;
-        break;
-      case '#':
-          Keyboard.press(KEY_LEFT_GUI);
-          Keyboard.write('r');
-          Keyboard.releaseAll();
-          delay(200);
-          Keyboard.println("cmd.exe");
-          delay(1000);
-          Keyboard.println("dir");
-          break;
+  if (customKey)
+  {
+    switch (customKey)
+    {
+    case 'A':
+      bourinage = false;
+      break;
+    case 'B':
+      bourinage = true;
+      break;
+    case '1':
+      if (clickDelay < 500)
+      {
+        clickDelay += 10;
+      }
+      break;
+    case '4':
+      if (clickDelay > 10)
+      {
+        clickDelay -= 10;
+      }
+      break;
+    case '2':
+      mouseButton = MOUSE_LEFT;
+      break;
+    case '5':
+      mouseButton = MOUSE_RIGHT;
+      break;
+    case '#':
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.write('r');
+      Keyboard.releaseAll();
+      delay(200);
+      Keyboard.println("cmd.exe");
+      delay(1000);
+      Keyboard.println("dir");
+      break;
     }
     Serial.println(customKey);
 
     refreshScreen();
   }
 
-  if ((millis() - lastUpdate) > clickDelay) {
-    if (bourinage) {
+  if ((millis() - lastUpdate) > clickDelay)
+  {
+    if (bourinage)
+    {
       Mouse.click(mouseButton);
     }
     lastUpdate = millis();
   }
 
-  if (millis() - blinkUpdate > clickDelay * 10) {
-    if (bourinage) {
+  if (millis() - blinkUpdate > clickDelay * 10)
+  {
+    if (bourinage)
+    {
       digitalWrite(LED_BUILTIN, ledState);
-      if (ledState == HIGH) {
+      if (ledState == HIGH)
+      {
         ledState = LOW;
-      } else {
+      }
+      else
+      {
         ledState = HIGH;
       }
       blinkUpdate = millis();
